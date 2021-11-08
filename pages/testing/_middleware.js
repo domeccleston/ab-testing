7fe456c1ac072d35d074f2fddc888462870e378c
Dom Eccleston <domeccleston@gmail.com>
2021-11-08 10:59:40 +0000

    Allow visitors to be A/B tested based on initial URL

--- pages/testing/_middleware.js
+++ pages/testing/_middleware.js
@@ -4,8 +4,20 @@ import { BUCKETS, getRandomBucket } from '../../lib/buckets'
 const COOKIE_NAME = 'landing-ab';
 
 export function middleware(req) {
-  // Get the bucket cookie
-  const bucket = req.cookies[COOKIE_NAME] || getRandomBucket(BUCKETS)
+  let bucket;
+
+  const urlParts = req.url.split('/');
+  const path = urlParts[urlParts.length - 1];
+  const hasUrlVariant = BUCKETS.includes(path.toUpperCase());
+
+  if (req.cookies[COOKIE_NAME]) {
+    bucket = req.cookies[COOKIE_NAME];
+  } else if (hasUrlVariant) {
+    bucket = path.toUpperCase();
+  } else {
+    bucket = getRandomBucket(BUCKETS);
+  }
+
   const res = NextResponse.rewrite(`/testing/${bucket}`)
 
   // Add the bucket to cookies if it's not there
